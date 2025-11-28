bc6342a69f789e9f89ece17ac104d24b0f4cce7a
EtienneDumai <etienne.dumai@gmail.com>
2025-11-28 10:46:06 +0100

    ajout du 3eme point

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print(df)
 corr_depart = df_num.corr()
 
 plt.figure(figsize=(8, 6))
-sns.heatmap(corr_depart, annot=True, vmin=-1, vmax=1, cmap="coolwarm")
+sns.heatmap(corr_depart, annot=False, vmin=-1, vmax=1, cmap="coolwarm")
 plt.title("Matrice de corrélation - données brutes")
 plt.tight_layout()
 plt.show()
@@ -20,7 +20,17 @@ df_cr = (df_num - df_num.mean()) / df_num.std(ddof=0)
 corr_cr = df_cr.corr()
 
 plt.figure(figsize=(8, 6))
-sns.heatmap(corr_cr, annot=True, vmin=-1, vmax=1, cmap="coolwarm")
+sns.heatmap(corr_cr, annot=False, vmin=-1, vmax=1, cmap="coolwarm")
 plt.title("Matrice de corrélation - données centrées-réduites")
 plt.tight_layout()
 plt.show()
+
+
+cov_depart = df_num.cov()
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(cov_depart, annot=False, cmap="viridis")
+plt.title("Matrice de variance-covariance - données brutes")
+plt.tight_layout()
+plt.show()
+
